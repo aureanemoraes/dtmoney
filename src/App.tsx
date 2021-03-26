83adcb7621e9f110b199a8ee6c57102ea1aa8132
aureane moraes <aureane.moraes@outlook.com>
2021-03-26 17:45:02 -0300

    checkbox style finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { Header } from './components/Header';
 import {GlobalStyle} from './styles/global';
 import Modal from 'react-modal';
 import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
-Modal.setAppElement('#root');
+Modal.setAppElement('#root');  
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -20,12 +21,10 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
-      <Modal
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar Transação</h2>
-        </Modal>
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <Dashboard />
       <GlobalStyle />
     </>
